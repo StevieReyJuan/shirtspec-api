30e6f98d4d3f9dbfe9d6eca078c444c29dc0051d
S. Steven Reyes <s.steven.reyes@gmail.com>
2020-07-19 19:40:58 -0400

    CORS typo?

--- src/config.js
+++ src/config.js
@@ -5,5 +5,5 @@ module.exports = {
     TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://sreyes@localhost/shirtspec-test',
     JWT_SECRET: process.env.JWT_SECRET || 'test-jwt-secret',
     JWT_EXPIRY: process.env.JWT_EXPIRY || '3h',
-    CLIENT_ORIGIN: 'https://shirtspec-app.vercel.app/'
+    CLIENT_ORIGIN: 'https://shirtspec-app.vercel.app'
 }
